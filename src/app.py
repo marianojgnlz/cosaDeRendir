49353ca491674c3502e27a165c47df8c350175e7
Mariano Gonzalez <mariano.gonzalez@urbetrack.com>
2022-06-12 20:50:53 -0300

    Arreglo los errores

--- src/app.py
+++ src/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Esto es un cambio"
+    return "Esto si anda."
 if __name__ == "__main__":
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from app import index
 
 def test_index():
-    assert index() == "Hello, it's me!"
+    assert index() == "Esto si anda."
